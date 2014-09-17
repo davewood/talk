f31f40d784b5d0f1dbf8fe37c06d998d5e4b3baf
David Schmidt <mail@davidschmidt.at>
2014-09-17 15:42:21 +0200

    remove boilerplate comments/POD

--- lib/Talk.pm
+++ lib/Talk.pm
@@ -4,18 +4,6 @@ use namespace::autoclean;
 
 use Catalyst::Runtime 5.80;
 
-# Set flags and add plugins for the application.
-#
-# Note that ORDERING IS IMPORTANT here as plugins are initialized in order,
-# therefore you almost certainly want to keep ConfigLoader at the head of the
-# list if you're using it.
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
 use Catalyst qw/
     ConfigLoader
     Static::Simple
@@ -31,21 +19,11 @@ extends 'Catalyst';
 
 our $VERSION = '0.01';
 
-# Configure the application.
-#
-# Note that settings in talk.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
 
 __PACKAGE__->config(
     name => 'Talk',
-
-    # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
-    enable_catalyst_header                      => 1,   # Send X-Catalyst header
+    enable_catalyst_header                      => 1,
     'Plugin::Session' => { flash_to_stash => 1 },
     'View::HTML'      => {
         TEMPLATE_EXTENSION => '.tt',
@@ -102,35 +80,5 @@ __PACKAGE__->config(
     },
 );
 
-# Start the application
 __PACKAGE__->setup();
-
-
-=head1 NAME
-
-Talk - Catalyst based application
-
-=head1 SYNOPSIS
-
-    script/talk_server.pl
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 SEE ALSO
-
-L<Talk::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-David Schmidt,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
 1;
--- lib/Talk/Controller/Root.pm
+++ lib/Talk/Controller/Root.pm
@@ -3,38 +3,11 @@ use Moose;
 use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' }
-
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config(namespace => '');
-
-=head1 NAME
-
-Talk::Controller::Root - Root Controller for Talk
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=head2 index
-
-The root page (/)
-
-=cut
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 }
-
-=head2 default
-
-Standard 404 error page
-
-=cut
 
 sub default :Path {
     my ( $self, $c ) = @_;
@@ -51,25 +24,7 @@ sub error : Private {
     $c->stash(template => 'error.tt');
 }
 
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut
-
 sub end : ActionClass('RenderView') {}
 
-=head1 AUTHOR
-
-David Schmidt,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
 __PACKAGE__->meta->make_immutable;
-
 1;
