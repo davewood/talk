9faef2c95091056206df7660cf10f064a3c30ac9
David Schmidt <mail@davidschmidt.at>
2014-09-25 09:09:40 +0200

    perltidy, rm password

--- lib/Talk.pm
+++ lib/Talk.pm
@@ -22,12 +22,12 @@ our $VERSION = '0.01';
 
 
 __PACKAGE__->config(
-    name => 'Talk',
-    encoding => 'UTF-8',
+    name                                        => 'Talk',
+    encoding                                    => 'UTF-8',
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header                      => 1,
-    'Plugin::Session' => { flash_to_stash => 1 },
-    'View::HTML'      => {
+    'Plugin::Session'                           => { flash_to_stash => 1 },
+    'View::HTML'                                => {
         TEMPLATE_EXTENSION => '.tt',
         render_die         => 1,
         INCLUDE_PATH       => [ __PACKAGE__->path_to(qw/ root templates /) ],
@@ -52,12 +52,12 @@ __PACKAGE__->config(
                 class => 'Minimal',
                 users => {
                     admin => {
-                        password => "asdfasdf8",
+                        password => '',
                     },
-                }
-            }
-       }
-     },
+                },
+            },
+        },
+    },
     'Controller::Login' => {
         login_form_args => {
             field_list => [ remember => { inactive => 1 } ],
